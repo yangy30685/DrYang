29547cc58091882c2bf8c78d4f135157d984b19b
dryang <yangy30685@live.co.uk>
2019-03-08 16:34:21 +0800

    update

--- linux_shell/Denyhosts.sh
+++ linux_shell/Denyhosts.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Denyhosts SHELL SCRIPT
-cat /var/log/secure|awk '/Invalid/{print $(NF-2)}'|sort|uniq -c|awk '{print $2"="$1;}' >/root/drYangSpot/linux_shell/Denyhosts.txt
+cat /var/log/secure|awk '/Invalid/{print $(NF-2)}'|sort|uniq -c|awk '{print $2"="$1;}' >/root/dryang/linux_shell/Denyhosts.txt
 DEFINE="5"
-for i in `cat /root/drYangSpot/linux_shell/Denyhosts.txt`
+for i in `cat /root/dryang/linux_shell/Denyhosts.txt`
 do
         IP=`echo $i|awk -F= '{print $1}'`
         NUM=`echo $i|awk -F= '{print $2}'`
