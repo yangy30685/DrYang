79bca7720590330e8a6dfd7434927a90f34b4fea
dryang <yangy30685@live.co.uk>
2019-03-15 10:52:26 +0800

    update

--- linux_shell/install_app.sh
+++ linux_shell/install_app.sh
@@ -2,26 +2,28 @@
 
 #root log in
 if [ $UID -ne 0 ];then
-	echo "error! change to root!"
+	echo
+	echo "Error! change to root!"
 exit 1
 fi
-	echo "please wait..."
-	echo #empty line
-	apt-get update
-	echo "now its upgrading!!!"
-	apt-get dist-upgrade -y
+
+echo "Please wait..."
+echo 
+apt-get update
+DATE=`date +%Y-%d-%M`
+echo "------------ ${DATE} --------------------"
+echo "now its upgrading!!!"
+apt-get dist-upgrade -y
 
 if [ $? -eq 0 ]; then
 	echo "the update is finished!"
 fi
 
-	echo
-	echo "now clean the file"
-	apt-get clean
+echo
+echo "now clean the file"
+apt-get clean && apt-get autoremove
 
 #suppres the error using 2>&1
-
-	dpkg --list | grep "^rc" | cut -d " " -f 3| xargs dpkg --purge >/dev/null 2>&1
-	echo "FINISH!"	
-	exit 0
-
+dpkg --list | grep "^rc" | cut -d " " -f 3 | xargs dpkg --purge >/dev/null 2>&1
+echo "FINISH!"	
+exit 0
